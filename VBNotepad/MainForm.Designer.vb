938b32176d754d097e8cf1bb51c6b0a40c0ebd0c
APIBox <LinkerVB@outlook.com>
2024-01-27 13:33:32 +0800

    上传

---
+++ VBNotepad/MainForm.Designer.vb
@@ -0,0 +1,351 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class VBNotepad
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(VBNotepad))
+        MovePanel = New Panel()
+        CloseBtn = New Label()
+        MinWindow = New Label()
+        TC = New TabControl()
+        主页 = New TabPage()
+        NewPage = New Label()
+        FileOption = New Label()
+        FileMenu = New Panel()
+        SaveAsBtn = New Label()
+        SaveBtn = New Label()
+        OpenBtn = New Label()
+        OpenFileDialog1 = New OpenFileDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        Edit = New Label()
+        EditMenu = New Panel()
+        replaceString = New Label()
+        EditFont = New Label()
+        FindText = New Label()
+        FontDialog1 = New FontDialog()
+        Look = New Label()
+        LookMenu = New Panel()
+        Label1 = New Label()
+        Label2 = New Label()
+        WordWrap = New Label()
+        MaxWindow = New Label()
+        MovePanel.SuspendLayout()
+        TC.SuspendLayout()
+        FileMenu.SuspendLayout()
+        EditMenu.SuspendLayout()
+        LookMenu.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MovePanel
+        ' 
+        MovePanel.BackColor = Color.DimGray
+        MovePanel.Controls.Add(MaxWindow)
+        MovePanel.Controls.Add(CloseBtn)
+        MovePanel.Controls.Add(MinWindow)
+        MovePanel.Location = New Point(0, 1)
+        MovePanel.Name = "MovePanel"
+        MovePanel.Size = New Size(743, 40)
+        MovePanel.TabIndex = 0
+        ' 
+        ' CloseBtn
+        ' 
+        CloseBtn.AutoSize = True
+        CloseBtn.Font = New Font("黑体", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        CloseBtn.ForeColor = SystemColors.ControlText
+        CloseBtn.Location = New Point(701, 8)
+        CloseBtn.Name = "CloseBtn"
+        CloseBtn.Size = New Size(38, 25)
+        CloseBtn.TabIndex = 2
+        CloseBtn.Text = "×"
+        ' 
+        ' MinWindow
+        ' 
+        MinWindow.AutoSize = True
+        MinWindow.Font = New Font("黑体", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        MinWindow.ForeColor = SystemColors.ControlText
+        MinWindow.Location = New Point(639, 8)
+        MinWindow.Name = "MinWindow"
+        MinWindow.Size = New Size(25, 25)
+        MinWindow.TabIndex = 1
+        MinWindow.Text = "-"
+        ' 
+        ' TC
+        ' 
+        TC.Controls.Add(主页)
+        TC.Location = New Point(0, 70)
+        TC.Name = "TC"
+        TC.SelectedIndex = 0
+        TC.Size = New Size(743, 656)
+        TC.TabIndex = 1
+        ' 
+        ' 主页
+        ' 
+        主页.BackColor = Color.Silver
+        主页.Location = New Point(4, 29)
+        主页.Name = "主页"
+        主页.Size = New Size(735, 623)
+        主页.TabIndex = 0
+        主页.Text = ":)"
+        ' 
+        ' NewPage
+        ' 
+        NewPage.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        NewPage.ForeColor = SystemColors.ControlText
+        NewPage.Location = New Point(678, 44)
+        NewPage.Name = "NewPage"
+        NewPage.Size = New Size(58, 23)
+        NewPage.TabIndex = 3
+        NewPage.Text = "+"
+        NewPage.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' FileOption
+        ' 
+        FileOption.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        FileOption.ForeColor = SystemColors.ControlText
+        FileOption.ImageAlign = ContentAlignment.BottomLeft
+        FileOption.Location = New Point(7, 44)
+        FileOption.Name = "FileOption"
+        FileOption.Size = New Size(66, 28)
+        FileOption.TabIndex = 4
+        FileOption.Text = "文件"
+        ' 
+        ' FileMenu
+        ' 
+        FileMenu.BackColor = Color.LightGray
+        FileMenu.Controls.Add(SaveAsBtn)
+        FileMenu.Controls.Add(SaveBtn)
+        FileMenu.Controls.Add(OpenBtn)
+        FileMenu.Location = New Point(7, 70)
+        FileMenu.Name = "FileMenu"
+        FileMenu.Size = New Size(132, 139)
+        FileMenu.TabIndex = 5
+        ' 
+        ' SaveAsBtn
+        ' 
+        SaveAsBtn.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        SaveAsBtn.ForeColor = SystemColors.ControlText
+        SaveAsBtn.ImageAlign = ContentAlignment.BottomLeft
+        SaveAsBtn.Location = New Point(9, 96)
+        SaveAsBtn.Name = "SaveAsBtn"
+        SaveAsBtn.Size = New Size(123, 24)
+        SaveAsBtn.TabIndex = 8
+        SaveAsBtn.Text = "另存为"
+        ' 
+        ' SaveBtn
+        ' 
+        SaveBtn.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        SaveBtn.ForeColor = SystemColors.ControlText
+        SaveBtn.ImageAlign = ContentAlignment.BottomLeft
+        SaveBtn.Location = New Point(9, 56)
+        SaveBtn.Name = "SaveBtn"
+        SaveBtn.Size = New Size(120, 24)
+        SaveBtn.TabIndex = 7
+        SaveBtn.Text = "保存"
+        ' 
+        ' OpenBtn
+        ' 
+        OpenBtn.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        OpenBtn.ForeColor = SystemColors.ControlText
+        OpenBtn.ImageAlign = ContentAlignment.BottomLeft
+        OpenBtn.Location = New Point(9, 18)
+        OpenBtn.Name = "OpenBtn"
+        OpenBtn.Size = New Size(126, 24)
+        OpenBtn.TabIndex = 6
+        OpenBtn.Text = "打开"
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' Edit
+        ' 
+        Edit.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Edit.ForeColor = SystemColors.ControlText
+        Edit.ImageAlign = ContentAlignment.BottomLeft
+        Edit.Location = New Point(79, 44)
+        Edit.Name = "Edit"
+        Edit.Size = New Size(66, 28)
+        Edit.TabIndex = 6
+        Edit.Text = "编辑"
+        ' 
+        ' EditMenu
+        ' 
+        EditMenu.BackColor = Color.LightGray
+        EditMenu.Controls.Add(replaceString)
+        EditMenu.Controls.Add(EditFont)
+        EditMenu.Controls.Add(FindText)
+        EditMenu.Location = New Point(79, 70)
+        EditMenu.Name = "EditMenu"
+        EditMenu.Size = New Size(132, 139)
+        EditMenu.TabIndex = 9
+        ' 
+        ' replaceString
+        ' 
+        replaceString.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        replaceString.ForeColor = SystemColors.ControlText
+        replaceString.ImageAlign = ContentAlignment.BottomLeft
+        replaceString.Location = New Point(9, 96)
+        replaceString.Name = "replaceString"
+        replaceString.Size = New Size(123, 24)
+        replaceString.TabIndex = 8
+        replaceString.Text = "替换"
+        ' 
+        ' EditFont
+        ' 
+        EditFont.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        EditFont.ForeColor = SystemColors.ControlText
+        EditFont.ImageAlign = ContentAlignment.BottomLeft
+        EditFont.Location = New Point(9, 56)
+        EditFont.Name = "EditFont"
+        EditFont.Size = New Size(120, 24)
+        EditFont.TabIndex = 7
+        EditFont.Text = "字体"
+        ' 
+        ' FindText
+        ' 
+        FindText.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        FindText.ForeColor = SystemColors.ControlText
+        FindText.ImageAlign = ContentAlignment.BottomLeft
+        FindText.Location = New Point(9, 18)
+        FindText.Name = "FindText"
+        FindText.Size = New Size(126, 24)
+        FindText.TabIndex = 6
+        FindText.Text = "查找"
+        ' 
+        ' Look
+        ' 
+        Look.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Look.ForeColor = SystemColors.ControlText
+        Look.ImageAlign = ContentAlignment.BottomLeft
+        Look.Location = New Point(151, 44)
+        Look.Name = "Look"
+        Look.Size = New Size(69, 28)
+        Look.TabIndex = 10
+        Look.Text = "查看"
+        ' 
+        ' LookMenu
+        ' 
+        LookMenu.BackColor = Color.LightGray
+        LookMenu.Controls.Add(Label1)
+        LookMenu.Controls.Add(Label2)
+        LookMenu.Controls.Add(WordWrap)
+        LookMenu.Location = New Point(151, 70)
+        LookMenu.Name = "LookMenu"
+        LookMenu.Size = New Size(168, 139)
+        LookMenu.TabIndex = 10
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ControlText
+        Label1.ImageAlign = ContentAlignment.BottomLeft
+        Label1.Location = New Point(9, 96)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(123, 24)
+        Label1.TabIndex = 8
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.ForeColor = SystemColors.ControlText
+        Label2.ImageAlign = ContentAlignment.BottomLeft
+        Label2.Location = New Point(9, 56)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(120, 24)
+        Label2.TabIndex = 7
+        ' 
+        ' WordWrap
+        ' 
+        WordWrap.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        WordWrap.ForeColor = SystemColors.ControlText
+        WordWrap.ImageAlign = ContentAlignment.BottomLeft
+        WordWrap.Location = New Point(9, 18)
+        WordWrap.Name = "WordWrap"
+        WordWrap.Size = New Size(222, 24)
+        WordWrap.TabIndex = 6
+        WordWrap.Text = "√自动换行"
+        ' 
+        ' MaxWindow
+        ' 
+        MaxWindow.AutoSize = True
+        MaxWindow.Font = New Font("黑体", 12.5F, FontStyle.Regular, GraphicsUnit.Point)
+        MaxWindow.ForeColor = SystemColors.ControlText
+        MaxWindow.Location = New Point(670, 8)
+        MaxWindow.Name = "MaxWindow"
+        MaxWindow.Size = New Size(32, 21)
+        MaxWindow.TabIndex = 3
+        MaxWindow.Text = "口"
+        ' 
+        ' VBNotepad
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DarkGray
+        ClientSize = New Size(745, 722)
+        Controls.Add(LookMenu)
+        Controls.Add(Look)
+        Controls.Add(EditMenu)
+        Controls.Add(Edit)
+        Controls.Add(FileMenu)
+        Controls.Add(FileOption)
+        Controls.Add(NewPage)
+        Controls.Add(TC)
+        Controls.Add(MovePanel)
+        FormBorderStyle = FormBorderStyle.None
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "VBNotepad"
+        Text = "VBNotepad"
+        MovePanel.ResumeLayout(False)
+        MovePanel.PerformLayout()
+        TC.ResumeLayout(False)
+        FileMenu.ResumeLayout(False)
+        EditMenu.ResumeLayout(False)
+        LookMenu.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents MovePanel As Panel
+    Friend WithEvents TC As TabControl
+    Friend WithEvents 主页 As TabPage
+    Friend WithEvents MinWindow As Label
+    Friend WithEvents CloseBtn As Label
+    Friend WithEvents NewPage As Label
+    Friend WithEvents FileOption As Label
+    Friend WithEvents FileMenu As Panel
+    Friend WithEvents OpenBtn As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents SaveBtn As Label
+    Friend WithEvents SaveAsBtn As Label
+    Friend WithEvents Edit As Label
+    Friend WithEvents EditMenu As Panel
+    Friend WithEvents replaceString As Label
+    Friend WithEvents EditFont As Label
+    Friend WithEvents FindText As Label
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents Look As Label
+    Friend WithEvents LookMenu As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents WordWrap As Label
+    Friend WithEvents MaxWindow As Label
+End Class
---
+++ VBNotepad/MainForm.vb
@@ -0,0 +1,343 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System
+Imports System.Drawing
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms
+
+Public Class VBNotepad
+
+    Declare Auto Function ReleaseCapture Lib "user32.dll" Alias "ReleaseCapture" () As Boolean
+    'API ReleaseCapture函数是用来释放鼠标捕获的
+    Declare Auto Function SendMessage Lib "user32.dll" Alias "SendMessage" (ByVal hWnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As IntPtr
+    '向windows发送消息
+    Public Const WM_SYSCOMMAND As Integer = &H112&
+    Public Const SC_MOVE As Integer = &HF010&
+    Public Const HTCAPTION As Integer = &H2&
+
+    Public FileNameLabel = New Label
+    Public Page = New TabPage
+    WithEvents RichEdit As New RichTextBox
+    Dim isMouseDown As Boolean = False
+    Dim direction As MouseDirection = MouseDirection.None
+    Dim mouseOff As Point
+    ' Public config_file As New StreamReader(Application.StartupPath & "\config.ini", Encoding.UTF8)
+    'Public config() As String = {}
+    Public Enum MouseDirection
+        Herizontal
+        Vertical
+        Declining
+        None
+    End Enum
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseDown
+        mouseOff = New Point(-e.X, -e.Y)
+        If e.Location.X >= Me.Width - 5 AndAlso e.Location.Y > Me.Height - 5 Then
+            isMouseDown = True
+        ElseIf e.Location.X >= Me.Width - 5 Then
+            isMouseDown = True
+        ElseIf e.Location.Y >= Me.Height - 5 Then
+            isMouseDown = True
+        Else
+            Me.Cursor = Cursors.Arrow
+            '改变鼠标样式为原样
+            isMouseDown = False
+            '鼠标移动事件
+            ReleaseCapture()
+            SendMessage(Me.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0)
+        End If
+    End Sub
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseMove
+        'ReleaseCapture()
+        'SendMessage(Me.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0)
+        '鼠标移动到边缘，改变鼠标的图标
+        If e.Location.X >= Me.Width - 5 AndAlso e.Location.Y > Me.Height - 5 Then
+            Me.Cursor = Cursors.SizeNWSE
+            direction = MouseDirection.Declining
+        ElseIf e.Location.X >= Me.Width - 5 Then
+            Me.Cursor = Cursors.SizeWE
+            direction = MouseDirection.Herizontal
+        ElseIf e.Location.Y >= Me.Height - 5 Then
+            Me.Cursor = Cursors.SizeNS
+            direction = MouseDirection.Vertical
+        Else
+            '否则，以外的窗体区域，鼠标星座均为单向箭头（默认）             
+            Me.Cursor = Cursors.Arrow
+        End If
+        If e.Location.X >= (Me.Width + Me.Left + 10) OrElse (e.Location.Y > Me.Height + Me.Top + 10) Then
+            isMouseDown = False
+        End If
+
+        '设定好方向后，调用下面方法，改变窗体大小  
+        ResizeWindow()
+
+    End Sub
+    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseUp
+        '  Console.WriteLine("松开鼠标")
+        isMouseDown = False
+        direction = MouseDirection.None
+        If isMouseDown Then
+            isMouseDown = False
+        End If
+    End Sub
+    Private Sub MinWindow_Click(sender As Object, e As EventArgs) Handles MinWindow.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub rm1_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MovePanel.MouseMove
+        If e.Button = MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Me.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0)
+        End If
+
+
+    End Sub
+
+    Private Sub NewPage_Click(sender As Object, e As EventArgs) Handles NewPage.Click
+        NewTabPage(1)
+    End Sub
+
+    Public Sub NewTabPage(index As Integer)
+        Dim rnd As New Random
+        Page = New TabPage
+        RichEdit = New RichTextBox
+        FileNameLabel = New Label
+
+        With Page
+            .BackColor = Color.Silver
+            .Name = "NewPage" & rnd.Next()
+            .text = "未命名"
+        End With
+        With RichEdit
+            .BackColor = Color.Silver
+            .Name = "NewRichEdit" & rnd.Next()
+            .Size = Size
+            .AllowDrop = True
+        End With
+
+        If index = 2 Then
+            If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+                Dim filename = OpenFileDialog1.FileName
+                Dim FileOpen As New StreamReader(filename)
+                RichEdit.Text = FileOpen.ReadToEnd()
+                FileOpen.Close()
+                Page.text = filename.Substring(filename.LastIndexOf("\") + 1)
+                With FileNameLabel
+                    .text = OpenFileDialog1.FileName
+                    .visible = False
+                End With
+            End If
+        ElseIf index = 1 Then
+            Try
+                Dim filename = Command()
+                Dim FileOpen As New StreamReader(filename)
+                RichEdit.Text = FileOpen.ReadToEnd()
+                FileOpen.Close()
+                Page.text = filename.Substring(filename.LastIndexOf("\") + 1)
+                With FileNameLabel
+                    .text = Command()
+                    .visible = False
+                End With
+            Catch ex As Exception
+
+            End Try
+        End If
+
+
+
+
+        TC.Controls.Add(Page)
+        Page.Controls.Add(RichEdit)
+        Page.Controls.Add(FileNameLabel)
+        TC.SelectedTab = Page
+    End Sub
+    Public Sub Save(index As Integer)
+        If index = 1 Then
+            If TC.SelectedTab.Text = "未命名" Then
+                If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+                    Dim SaveAsFileName = SaveFileDialog1.FileName
+                    Dim FileWrite As New StreamWriter(SaveAsFileName)
+                    FileWrite.Write(TC.SelectedTab.Controls.Item(0).Text)
+                    FileWrite.Close()
+                End If
+            Else
+                If TC.SelectedIndex <> 0 Then
+                    Dim SaveFileName = TC.SelectedTab.Controls.Item(1).Text
+                    Dim FileWrite2 As New StreamWriter(SaveFileName)
+                    FileWrite2.Write(TC.SelectedTab.Controls.Item(0).Text)
+                    FileWrite2.Close()
+                End If
+            End If
+        ElseIf index = 2 Then
+            If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+                Dim SaveAsFileName = SaveFileDialog1.FileName
+                Dim FileWrite As New StreamWriter(SaveAsFileName)
+                FileWrite.Write(TC.SelectedTab.Controls.Item(0).Text)
+                FileWrite.Close()
+            End If
+        End If
+    End Sub
+    Private Sub TC_DoubleClick(sender As Object, e As EventArgs) Handles TC.DoubleClick
+        Dim Temp_SelTab = TC.SelectedTab
+        Dim Temp_SelIndex = TC.SelectedIndex
+        TC.TabPages.Remove(Temp_SelTab)
+        If TC.SelectedIndex <> 0 Then
+            TC.SelectedIndex = Temp_SelIndex - 1
+        Else
+            TC.SelectedIndex = Temp_SelIndex
+        End If
+    End Sub
+
+    Private Sub CloseBtn_Click(sender As Object, e As EventArgs) Handles CloseBtn.Click
+        For i = 0 To 10
+            Width -= 20
+            Height -= 20
+            Left += 10
+            Top += 10
+            Opacity -= 0.22
+        Next
+        End
+    End Sub
+
+    Private Sub VBNotepad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FileMenu.Hide()
+        EditMenu.Hide()
+        LookMenu.Hide()
+        If Command() <> "" Then
+            NewTabPage(1)
+        End If
+
+    End Sub
+
+    Private Sub FileOption_Click(sender As Object, e As EventArgs) Handles FileOption.Click
+        If FileMenu.Visible Then
+            FileMenu.Hide()
+            FileOption.BackColor = Color.DarkGray
+        Else
+            FileMenu.Show()
+            FileOption.BackColor = Color.LightGray
+        End If
+    End Sub
+
+    Private Sub OpenBtn_Click(sender As Object, e As EventArgs) Handles OpenBtn.Click
+        NewTabPage(2)
+    End Sub
+
+    Private Sub SaveBtn_Click(sender As Object, e As EventArgs) Handles SaveBtn.Click
+        Save(1)
+    End Sub
+
+    Private Sub SaveAsBtn_Click(sender As Object, e As EventArgs) Handles SaveAsBtn.Click
+        Save(2)
+    End Sub
+
+    Private Sub VBNotepad_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        NewPage.Location = New Point(Width - 67, NewPage.Location.Y)
+        CloseBtn.Location = New Point(Width - 44, CloseBtn.Location.Y)
+        MaxWindow.Location = New Point(Width - 75, MinWindow.Location.Y)
+        MinWindow.Location = New Point(Width - 106, MinWindow.Location.Y)
+        MovePanel.Width = Width
+        TC.Size = New Size(Size.Width, Size.Height)
+        RichEdit.Size = New Size(Size.Width, Size.Height)
+    End Sub
+
+    Private Sub Edit_Click(sender As Object, e As EventArgs) Handles Edit.Click
+        If EditMenu.Visible Then
+            EditMenu.Hide()
+            Edit.BackColor = Color.DarkGray
+        Else
+            EditMenu.Show()
+            Edit.BackColor = Color.LightGray
+        End If
+    End Sub
+
+    Private Sub FindText_Click(sender As Object, e As EventArgs) Handles FindText.Click
+        Dim find = InputBox("输入需要寻找的文本", "寻找文本")
+        If find <> "" Then
+            RichEdit.Find(find)
+        End If
+    End Sub
+
+    Private Sub EditFont_Click(sender As Object, e As EventArgs) Handles EditFont.Click
+        If FontDialog1.ShowDialog = DialogResult.OK Then
+            Try
+                TC.SelectedTab.Controls.Item(0).Font = FontDialog1.Font
+            Catch
+            End Try
+        End If
+    End Sub
+
+    Private Sub ReplaceString_Click(sender As Object, e As EventArgs) Handles replaceString.Click
+        Dim old = InputBox("输入被替换的文本", "替换文本")
+        Dim replace = InputBox("输入替换的文本", "替换文本")
+        If replace <> "" Then
+            Try
+                RichEdit.Text = RichEdit.Text.Replace(old, replace)
+            Catch
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub ResizeWindow()
+
+        If Not isMouseDown Then
+            Return
+        End If
+        If direction = MouseDirection.Declining Then
+            'Me.Cursor = Cursors.SizeNWSE
+            '改变宽度
+            Me.Width = MousePosition.X - Me.Left + 5
+            Me.Height = MousePosition.Y - Me.Top + 5
+
+        ElseIf direction = MouseDirection.Herizontal Then
+            'Me.Cursor = Cursors.SizeWE
+            '改变宽度
+            Me.Width = MousePosition.X - Me.Left + 5
+
+        ElseIf direction = MouseDirection.Vertical Then
+            'Me.Cursor = Cursors.SizeNS
+            '改变高度
+            Me.Height = MousePosition.Y - Me.Top + 5
+
+        Else
+            '鼠标不在窗口右和下边缘，把鼠标打回原型
+            Me.Cursor = Cursors.Arrow
+            isMouseDown = False
+        End If
+        Refresh()
+    End Sub
+
+    Private Sub Look_Click(sender As Object, e As EventArgs) Handles Look.Click
+        If LookMenu.Visible Then
+            LookMenu.Hide()
+            Look.BackColor = Color.DarkGray
+        Else
+            LookMenu.Show()
+            Look.BackColor = Color.LightGray
+        End If
+
+    End Sub
+
+    Private Sub WordWrap_Click(sender As Object, e As EventArgs) Handles WordWrap.Click
+        Dim TempFont = RichEdit.Font
+        If RichEdit.WordWrap Then
+            RichEdit.WordWrap = False
+            WordWrap.Text = "自动换行"
+            RichEdit.Font = TempFont
+
+        Else
+            RichEdit.WordWrap = True
+            WordWrap.Text = "√自动换行"
+            RichEdit.Font = TempFont
+        End If
+    End Sub
+
+    Private Sub 主页_Click(sender As Object, e As EventArgs) Handles 主页.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles MaxWindow.Click
+        WindowState = FormWindowState.Maximized
+    End Sub
+End Class
